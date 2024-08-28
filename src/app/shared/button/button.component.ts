73e04b8ab2f1d8efcec57e1e31874c424ccc8ce3
alwyndy <alwyn.dy@synacy.com>
2024-08-29 04:45:30 +0800

    Add button component

---
+++ src/app/shared/button/button.component.ts
@@ -0,0 +1,24 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {NgClass} from "@angular/common";
+import {ButtonType} from "./button-type";
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [
+    NgClass
+  ],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+  @Input() public type!:ButtonType;
+  @Input() public isDisabled:boolean = false;
+  @Output() clickAction = new EventEmitter()
+
+  constructor() {}
+
+  public onClick(){
+    this.clickAction.emit()
+  }
+}
