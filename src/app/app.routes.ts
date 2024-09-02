f4ffd1a969c312c023f5659dfd004c93887bec02
alwyndy <alwyn.dy@synacy.com>
2024-09-02 16:17:16 +0800

    Add initial setup for pages authorization

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,17 @@
 import { Routes } from '@angular/router';
 import {WelcomeComponent} from "./welcome/welcome.component";
+import {UnauthorizedComponent} from "./unauthorized/unauthorized.component";
+import {hasRoleGuard} from "./has-role.guard";
+import {Role} from "./role";
 
 export const routes: Routes = [
-  {path: "**", component: WelcomeComponent}
+  {path: 'unauthorized', component: UnauthorizedComponent},
+  {
+    path: "welcome",
+    component: WelcomeComponent,
+    canActivate: [hasRoleGuard],
+    data: {
+      roles: [Role.HR_ADMIN]
+    }
+  }
 ];
---
+++ src/app/role.ts
@@ -0,0 +1,5 @@
+export enum Role {
+  EMPLOYEE,
+  MANAGER,
+  HR_ADMIN
+}
