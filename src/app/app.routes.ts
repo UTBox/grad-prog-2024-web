7a1cdfe54346b9e9e3700609fbb14b0d174e44a3
alwyndy <alwyn.dy@synacy.com>
2024-09-03 16:05:07 +0800

    Add get all leaves to route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import {WelcomeComponent} from "./welcome/welcome.component";
+import {AllLeavesComponent} from "./leaves/all-leaves/all-leaves.component";
 
 export const routes: Routes = [
-  {path: "**", component: WelcomeComponent}
+  {path: "leaves", children: [
+      {path: "", component: AllLeavesComponent}
+    ]},
+  {path: "**", redirectTo: "", component: WelcomeComponent}
 ];
