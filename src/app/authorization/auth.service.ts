1b1223598267f4373ff9b24eafa5bf370d64d79d
alwyndy <alwyn.dy@synacy.com>
2024-09-03 23:34:32 +0800

    Remove use of local storage

--- src/app/authorization/auth.service.ts
+++ src/app/authorization/auth.service.ts
@@ -1,18 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Role } from './role';
 import {UserObservable} from "./observable/user-observable";
-import {LocalStorageService} from "../shared/local-storage.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   constructor(
-    private userObservable:UserObservable,
-    private localStorage:LocalStorageService
+    private userObservable:UserObservable
   ) {}
 
   getUserRole(): Role{
-    return <Role> localStorage.getItem('role') ?? Role.NONE
   }
 }
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -3,7 +3,6 @@ import {RouterOutlet} from "@angular/router";
 import {Role} from "../authorization/role";
 import {FormControl, FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {UserObservable} from "../authorization/observable/user-observable";
-import {LocalStorageService} from "../shared/local-storage.service";
 
 interface User{
   id:number
@@ -56,18 +55,11 @@ export class SidebarComponent implements OnInit{
 
   constructor(
     private userObservable:UserObservable,
-    private localStorage:LocalStorageService
   ) {}
 
   ngOnInit() {
-    let storedUserId = localStorage.getItem("userId");
-    let storedUser = this.users.find(u => u.id === Number(storedUserId))
-    this.selectedUser.setValue(storedUser ?? this.defaultUser)
   }
 
   public handleChangeUser(){
-    localStorage.setItem('role', this.selectedUser.getRawValue()?.role.toString() ?? "")
-    localStorage.setItem('userId', this.selectedUser.getRawValue()?.id.toString() ?? "")
-    window.location.reload();
   }
 }
