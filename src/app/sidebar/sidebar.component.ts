35c76ccc85a099fb15eea5a69435f4f75444b4e5
alwyndy <alwyn.dy@synacy.com>
2024-09-04 04:16:33 +0800

    Fix dumb linking mistake

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -50,9 +50,9 @@ export class SidebarComponent implements OnInit{
     {
       label: "Leaves",
       pages: [
-        {label: "Apply", allowedRoles: [Role.MANAGER, Role.EMPLOYEE], link: "leaves/all"},
+        {label: "Apply", allowedRoles: [Role.MANAGER, Role.EMPLOYEE], link: ""},
         {label: "My Leaves", allowedRoles: [Role.MANAGER, Role.EMPLOYEE], link: ""},
-        {label: "View All", allowedRoles: [Role.MANAGER, Role.HR_ADMIN], link: ""},
+        {label: "View All", allowedRoles: [Role.MANAGER, Role.HR_ADMIN], link: "leaves/all"},
       ],
       allowedRoles: [Role.MANAGER, Role.HR_ADMIN, Role.EMPLOYEE]
     }
