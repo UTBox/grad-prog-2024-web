18b502af1844f7043b232ab8450ebd5b4bb2c6e7
seancapulong <sean.capulong@synacy.com>
2024-09-04 20:25:37 +0800

    Apply loading spinner to all employees component

--- src/app/employees/all-employees/all-employees.component.ts
+++ src/app/employees/all-employees/all-employees.component.ts
@@ -1,35 +1,32 @@
-import {Component, OnInit} from '@angular/core';
-import {PaginationComponent} from "../../shared/pagination/pagination.component";
-import {HttpParams} from "@angular/common/http";
-import {Location} from "@angular/common";
-import EmployeesService from "../data/employees.service";
-import PageResponse from "../../shared/page-response";
-import {lastValueFrom} from "rxjs";
-import {ButtonComponent} from "../../shared/button/button.component";
-import {ButtonStyle} from "../../shared/button/button-style";
+import { Component, OnInit } from '@angular/core';
+import { PaginationComponent } from '../../shared/pagination/pagination.component';
+import { HttpParams } from '@angular/common/http';
+import { Location } from '@angular/common';
+import EmployeesService from '../data/employees.service';
+import PageResponse from '../../shared/page-response';
+import { lastValueFrom } from 'rxjs';
+import { ButtonComponent } from '../../shared/button/button.component';
+import { ButtonStyle } from '../../shared/button/button-style';
 import { ActivatedRoute, Router } from '@angular/router';
+import { LoadingSpinnerComponent } from "../../shared/loading-spinner/loading-spinner.component";
 
 @Component({
   selector: 'app-all-employees',
   standalone: true,
-  imports: [
-    PaginationComponent,
-    ButtonComponent
-  ],
+  imports: [PaginationComponent, ButtonComponent, LoadingSpinnerComponent],
   templateUrl: './all-employees.component.html',
-  styleUrl: './all-employees.component.css'
+  styleUrl: './all-employees.component.css',
 })
-export class AllEmployeesComponent implements OnInit{
-  public isLoading = true
+export class AllEmployeesComponent implements OnInit {
+  public isLoading = true;
 
-  private max = 5
-  public currentPage = 1
-  public totalPages = 0
+  private max = 5;
+  public currentPage = 1;
+  public totalPages = 0;
 
-  public employees!:PageResponse<any>
+  public employees!: PageResponse<any>;
 
   protected readonly ButtonStyle = ButtonStyle;
-
 
   constructor(
     private employeesService: EmployeesService,
@@ -39,42 +36,45 @@ export class AllEmployeesComponent implements OnInit{
   ) {}
 
   ngOnInit() {
-    this.initializePage()
+    this.initializePage();
+  }
+
   onEdit(employeeId: string) {
     this.router.navigate([`/employees/edit/${employeeId}`]);
   }
 
-  public handleChangePage(page:number){
-    this.currentPage = page
-    this.setPageParams(page)
-    this.initializeData()
+  public handleChangePage(page: number) {
+    this.currentPage = page;
+    this.setPageParams(page);
+    this.initializeData();
   }
 
-  private async initializePage(){
-    this.getQueryParams()
-    await this.initializeData()
-    this.isLoading = false
+  private async initializePage() {
+    this.getQueryParams();
+    await this.initializeData();
+    this.isLoading = false;
   }
 
-  private async initializeData(){
-    this.employees = await lastValueFrom(this.employeesService.getPaginatedEmployees(this.max, this.currentPage))
-    this.totalPages = this.employees.totalPages
+  private async initializeData() {
+    this.employees = await lastValueFrom(
+      this.employeesService.getPaginatedEmployees(this.max, this.currentPage)
+    );
+    this.totalPages = this.employees.totalPages;
   }
 
-  private setPageParams(page: number){
+  private setPageParams(page: number) {
     let params = new HttpParams();
-    params = params.append("page", page);
+    params = params.append('page', page);
     this.location.go(this.location.path().split('?')[0], params.toString());
   }
 
-  private getQueryParams(){
-    this.route.queryParams.subscribe(params => {
-      if(isNaN(Number(params['page']))){
+  private getQueryParams() {
+    this.route.queryParams.subscribe((params) => {
+      if (isNaN(Number(params['page']))) {
         this.setPageParams(1);
         return;
       }
       this.currentPage = Number(params['page']);
     });
   }
-
 }
