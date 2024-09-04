00b75f655c38ae26390a9d2f3336be15278ebf86
alwyndy <alwyn.dy@synacy.com>
2024-09-04 20:22:41 +0800

    Add initial component

---
+++ src/app/leaves/my-leaves/my-leaves.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-leaves',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-leaves.component.html',
+  styleUrl: './my-leaves.component.css'
+})
+export class MyLeavesComponent {
+
+}
