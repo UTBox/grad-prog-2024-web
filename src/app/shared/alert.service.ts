4956802f9c50f2dbb981f1dec6d23c2479a7993a
seancapulong <sean.capulong@synacy.com>
2024-09-05 16:37:34 +0800

    Add approve and reject leave toasts alert in alert service

--- src/app/shared/alert.service.ts
+++ src/app/shared/alert.service.ts
@@ -7,7 +7,7 @@ import Swal from 'sweetalert2';
 export class AlertService {
   constructor() {}
 
-  showSuccess(message: string, title: string = 'Success') {
+  showSuccess(message: string, title: string = 'Success!') {
     Swal.fire({
       title: title,
       text: message,
@@ -18,14 +18,36 @@ export class AlertService {
     });
   }
 
-  showError(message: string, title: string = 'Error') {
+  showError(title: string, message: string) {
     Swal.fire({
-      title: title,
-      text: message,
+      title: title ? title : 'Error!',
+      text: message ? message : 'Unknown error',
       icon: 'error',
-      timer: 2000,
-      timerProgressBar: true,
       confirmButtonText: 'OK',
+    });
+  }
+
+  showApprovedLeaveToast() {
+    Swal.fire({
+      title: "Leave application approved!",
+      icon: "success",
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+    });
+  }
+
+  showRejectedLeaveToast() {
+    Swal.fire({
+      title: "Leave application rejected!",
+      icon: 'success',
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
     });
   }
 }
