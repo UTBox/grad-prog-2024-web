be86ed58799390aa25ed0fe000c7275e14c99fa1
seancapulong <sean.capulong@synacy.com>
2024-09-05 15:10:42 +0800

    Apply alert service to edit employee form submission

--- src/app/employees/edit-employee/edit-employee.component.ts
+++ src/app/employees/edit-employee/edit-employee.component.ts
@@ -11,6 +11,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import EmployeesService from '../data/employees.service';
 import { lastValueFrom } from 'rxjs';
 import { IEmployee } from '../model/employee.model';
+import { AlertService } from '../../shared/alert.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-edit-employee',
@@ -27,7 +29,8 @@ export class EditEmployeeComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private employeeService: EmployeesService,
-    private router: Router
+    private router: Router,
+    private alertService: AlertService
   ) {
     this.editEmployeeForm = new FormGroup({
       id: new FormControl(),
@@ -55,6 +58,7 @@ export class EditEmployeeComponent implements OnInit {
           this.router.navigate(['/employees/all'], {
             queryParams: { page: 1 },
           });
+          this.alertService.showSuccess("Successfully updated total leaves!");
         },
         error: (err) => {
           console.log(err);
